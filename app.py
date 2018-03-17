972edda6307d0661d986f0528fc10e828d6f0288
Your Name <you@example.com>
2018-03-17 19:06:59 +0200

    sdsdf

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from flask import Flask, request, render_template, redirect, url_for, session
 import os
+from math import ceil
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -13,7 +14,30 @@ app.secret_key = os.urandom(24)
 
 @app.route('/')
 def home():
-    avg =
-    return render_template('home.html')
+    context = {}
+    sum_c_min =
+    pre_avg_c_min =
+    avg_c_min = ceil(pre_avg_c_min)
+    context['c_min'] = avg_c_min
+    sum_c_max =
+    pre_avg_c_max =
+    avg_c_max = ceil(pre_avg_c_max)
+    context['c_max'] = avg_c_max
+    sum_c_current =
+    pre_avg_c_current =
+    avg_c_current = ceil(pre_avg_c_current)
+    context['c_current'] =
+    # видумати логіку вираховування статусу емодзі на основі середньої темпаратури ,опадів,приготувати емодзі
+    context['status'] = 5
+    sum_wind =
+    pre_avg_wind =
+    avg_wind = ceil(pre_avg_wind)
+    context['wind'] =6
+    context['c_feel'] ='загуглити як на основі погодних даних виводити :як відчувається'
+    sum_precipitation =
+    pre_avg_precipitation =
+    avg_precipitation = ceil(pre_avg_precipitation)
+    context['precipitation'] =8
+    return render_template('home.html',context=context)
 if __name__ == '__main__':
-    app.run(debug=True, port=7485)
+    app.run(debug=True, port=7778)

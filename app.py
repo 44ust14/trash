ef8376f3671cf6b9d6254829cad65dd8cc87de21
Lajob <ustym.hanyk@ukr.net>
2018-03-24 12:21:18 +0200

    average doneee

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
 from flask import Flask, request, render_template, redirect, url_for, session
 import os
-from math import ceil
+from statistics import mean as average
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -10,34 +11,23 @@ app.config['SECRET_KEY'] = 'secret!'
 # добавляємо секретний ключ для сайту щоб шифрувати дані сессії
 # при кожнаму сапуску фласку буде генечитись новий рандомний ключ з 24 символів
 app.secret_key = os.urandom(24)
-# app.secret_key = '125'
+app.secret_key = '125'
 
 @app.route('/')
 def home():
     context = {}
-    sum_c_min =
-    pre_avg_c_min =
-    avg_c_min = ceil(pre_avg_c_min)
-    context['c_min'] = avg_c_min
-    sum_c_max =
-    pre_avg_c_max =
-    avg_c_max = ceil(pre_avg_c_max)
-    context['c_max'] = avg_c_max
-    sum_c_current =
-    pre_avg_c_current =
-    avg_c_current = ceil(pre_avg_c_current)
-    context['c_current'] =
+    c_min_list =[1,2,3,445,5,6,7,8,9,10]
+    context['c_min'] = round(average(c_min_list))
+    c_max_list =[1,2,3,4,5,654,7,8,9,10]
+    context['c_max'] = round(average(c_max_list))
+    c_current_list =[1,2,3,4,5,654,7,8,9,10]
+    context['c_current'] = round(average(c_current_list))
     # видумати логіку вираховування статусу емодзі на основі середньої темпаратури ,опадів,приготувати емодзі
-    context['status'] = 5
-    sum_wind =
-    pre_avg_wind =
-    avg_wind = ceil(pre_avg_wind)
-    context['wind'] =6
+    wind_list =[1,223,334,4,5,65,7,8,9,10]
+    context['wind'] = round(average(wind_list))
     context['c_feel'] ='загуглити як на основі погодних даних виводити :як відчувається'
-    sum_precipitation =
-    pre_avg_precipitation =
-    avg_precipitation = ceil(pre_avg_precipitation)
-    context['precipitation'] =8
+    precipitation_list =[21,23,53,64,75,63,72,81,91,10]
+    context['precipitation'] = round(average(precipitation_list))
     return render_template('home.html',context=context)
 if __name__ == '__main__':
     app.run(debug=True, port=7778)
